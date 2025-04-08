cd7ed940efad08dd23d9be361cf81e29bb2bc5e6
Tommy Ogino <tommyogino275@gmail.com>
2025-04-07 23:25:45 -0300

    Changes to be committed: 	modified:   hello.go 	modified:   logs.txt

--- hello.go
+++ hello.go
@@ -119,7 +119,7 @@ func registraLog(site string, status bool) {
 		fmt.Println("error:", err)
 
 	}
-	arquivo.WriteString(site + " - online: " + strconv.FormatBool(status) + "\n")
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + "-" + site + " - online: " + strconv.FormatBool(status) + "\n")
 
 	arquivo.Close()
 }
